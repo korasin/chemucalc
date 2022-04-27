892bb27bfbec9f7bfa56eff790b3cba5457d1c54
korasin <mchemutai232@gmail.com>
2022-04-27 18:02:42 +0300

    functions

--- index.js
+++ index.js
@@ -7,3 +7,16 @@
 // function solve(){
 //     console.log(eval(result))
 // }
+ function display(val){
+     document.getElementById('result').value += val
+ }
+
+ function solve(){
+     let m = document.getElementById('result').value 
+     let v = eval(m)
+     document.getElementById('result').value = v
+ }
+
+ function allClear(){
+     document.getElementById('result').value = ''
+ }
